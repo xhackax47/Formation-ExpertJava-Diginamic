beac1ba8661466e4b432da651a087c21dfcb7f31
Samy Chaabi <xhackax47@gmail.com>
2018-09-17 12:55:41 +0200

    calculs

--- src/tppoo/Carre.java
+++ src/tppoo/Carre.java
@@ -5,14 +5,19 @@ public class Carre extends Rectangle implements Surfacable {
 	Point pointBasGauche;
 	int cote;
 	
-	public Carre(Point x, int y) {
-		this.pointBasGauche = x;
-		this.cote = y;
+	public Carre(Point p, int cote) {
+		this.pointBasGauche = p;
+		
+		this.cote = cote;
+		this.x = cote;
+		this.y = cote;
+		
 	}
 
 	@Override
 	public String toString() {
 		
+		Point pointBasGauche = this.getPointBasGauche();
 		Point pointBasDroit = new Point(pointBasGauche.getX(), pointBasGauche.getY() + cote);
 		Point pointHautDroit = new Point(pointBasGauche.getX() + cote, pointBasGauche.getY() + cote);
 		Point pointHautGauche = new Point(pointBasGauche.getX() + cote, pointBasGauche.getY());
